ba1a0a45e145c922b3771392a87347005f33a983
KipruttoMutai <mutaibrian04@gmail.com>
2023-11-16 18:10:39 +0000

    Nodes and Queues

---
+++ node.c
@@ -0,0 +1,62 @@
+#include " monty.h"
+/**
+ * addqueue - add node to the tail stack
+ * @n: new_value
+ * @head: head of the stack
+ * Return: no return
+ */
+void addqueue(stack_t **head, int n)
+{
+	stack_t *new_node, *current;
+
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		printf("Error\n");
+		return;
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new_node;
+		new_node->prev = NULL;
+		return;
+	}
+	current = *head;
+	while (current->next != NULL)
+	{
+		current = current->next;
+	}
+	current->next = new_node;
+	new_node->prev = current;
+}
+/**
+ * addnode - add node to the head stack
+ * @head: head of the stack
+ * @n: new_value
+ * Return: no return
+ */
+void addnode(stack_t **head, int n)
+{
+
+	stack_t *new_node;
+
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{ printf("Error\n");
+		exit(0);
+	}
+	new_node->n = n;
+	new_node->prev = NULL;
+	if (*head == NULL)
+	{
+		new_node->next = NULL;
+	}
+	else
+	{
+		new_node->next = *head;
+		(*head)->prev = new_node;
+	}
+	*head = new_node;
+}
