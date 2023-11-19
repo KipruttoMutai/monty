9eb417a2304aca4b35c5f0c8b69f50c325805180
KipruttoMutai <mutaibrian04@gmail.com>
2023-11-19 17:11:34 +0000

    Queues and Stacks

--- monty.h
+++ monty.h
@@ -81,5 +81,4 @@ void br_mul(stack_t **head, unsigned int counter);
 void br_pchar(stack_t **head, unsigned int counter);
 void br_pstr(stack_t **head, unsigned int counter);
 void br_rotl(stack_t **head,  __attribute__((unused)) unsigned int counter);
-
 #endif /*MONTY_H*/
---
+++ stqu.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+/**
+ * br_stack - prints the top
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void br_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	s_data.lifi = 0;
+}
+
+/**
+ * br_queue - prints the top
+ * @head: stack head
+ * @counter: line
+ * Return: Null
+*/
+void br_queue(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	s_data.lifi = 1;
+}
